09b91a3b7c05ee7b78662b843d5a510c0e67f41f
AntonioIndindoli <indindoliantonio@gmail.com>
2024-07-07 00:26:23 -0700

    prod prep

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const sharp = require("sharp");
 const crypto = require("crypto");
 const storage = multer.memoryStorage()
 const upload = multer();
+const path = require('path');
 
 // Create an express app
 const app = express();
@@ -124,6 +125,15 @@ app.put('/admin/verify-user/:userId', authenticate, isAdmin, (req, res) => {
       .catch(err => {
           res.status(500).json({ message: 'Error verifying user', error: err });
       });
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 // Start our server
