2fb8bf10850ba6a47d74ac63ba67320c04d196ce
AntonioIndindoli <aindindoli@mail.sfsu.edu>
2024-09-19 02:19:39 -0700

    env changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,5 @@
-// Load env variables
-if (process.env.NODE_ENV != "production") {
-  require("dotenv").config();
-}
+
+require("dotenv").config();
 
 // Import dependencies
 const Post = require("./models/post");
